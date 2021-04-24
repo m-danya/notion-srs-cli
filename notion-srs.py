3e68c667d6435b87bdd1ca436af54d41368f231a
m-danya <danila-mikh@ya.ru>
2021-04-24 12:39:41 +0300

    Add counting of correct answers

--- notion-srs.py
+++ notion-srs.py
@@ -8,6 +8,8 @@ import config as conf
 client = NotionClient(token_v2=conf.token_v2)
 cards = client.get_collection_view(conf.collection_url)
 
+correct = 0
+
 today_cards = cards.build_query(filter=cards.get('query2')['filter']).execute() 
 print(f"Hi! Cards for today: {len(today_cards)}\n")
 for card in today_cards:
@@ -22,6 +24,7 @@ for card in today_cards:
     c = input('(a = accept, d = decline, smth else = skip): ')
     if (c == 'a'):
         print('accepted.')
+        correct += 1
         if (card.get_property(conf.level) == '10'):
             print('maximum level reached! skipping..')
         else:
@@ -34,4 +37,6 @@ for card in today_cards:
         print()
         continue
     print()
+if (len(today_cards)):
+    print(f"Cards answered correctly: {correct}/{len(today_cards)}")
 print("That's all for today!")
